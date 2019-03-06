33e4f8cdfbb4c2811fc4cf28b8a0bc44d3029185
Aleksandr Levchuk <alevchuk@gmail.com>
2019-03-06 20:40:09 +0000

    Make version number the current git commit hash

--- biostar/__init__.py
+++ biostar/__init__.py
@@ -1,3 +1,7 @@
 from __future__ import absolute_import
+import subprocess
 
-VERSION = "16.09"
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
+VERSION = get_git_revision_short_hash()
