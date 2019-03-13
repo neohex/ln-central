7d35297915efceabd945dc713d6fbde2811d94aa
Aleksandr Levchuk <alevchuk@gmail.com>
2019-03-12 21:39:28 -0700

    More detail about last commit and webserver restart time in the website footer

--- biostar/__init__.py
+++ biostar/__init__.py
@@ -1,7 +1,18 @@
 from __future__ import absolute_import
 import subprocess
+from datetime import datetime
+from datetime import tzinfo, timedelta
+import pytz
+import dateutil.parser as dp
 
 def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+    out = subprocess.check_output(['git', 'show', '-s', '--format=%cI %h', 'HEAD']).decode('ascii').strip()
+    return out.split()
 
-VERSION = get_git_revision_short_hash()
+git_info = get_git_revision_short_hash() 
+VERSION ={
+	'build_time': dp.parse(git_info[0]),
+        'server_start_time': datetime.now(pytz.UTC),
+	'build_hash': git_info[1],
+    }
+
