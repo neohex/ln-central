6951af7908b627723161bd1174815caa4c89ca35
Aleksandr Levchuk <alevchuk@gmail.com>
2019-03-21 11:13:07 -0700

    Remove cookie requirements (auth/session/messages) + create live dir

--- writer-env/project-basedir/biostar_writer/settings.py
+++ writer-env/project-basedir/biostar_writer/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -31,21 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -60,8 +51,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -76,28 +65,14 @@ WSGI_APPLICATION = 'biostar_writer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'live', 'db.sqlite3'),
     }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []  # no password on this site
 
 
 # Internationalization
--- writer-env/project-basedir/manage.py
+++ writer-env/project-basedir/manage.py
@@ -2,7 +2,20 @@
 import os
 import sys
 
+def create_live_dir():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    LIVE_DIR = '{}/live'.format(BASE_DIR)
+    LIVE_DIR_ACCESS_RIGHTS = 0o755
+    
+    try:  
+        os.mkdir(LIVE_DIR, LIVE_DIR_ACCESS_RIGHTS)
+    except FileExistsError:
+        pass
+    else:  
+        print('Created new directory {}'.format(LIVE_DIR))
+
 if __name__ == '__main__':
+    create_live_dir()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biostar_writer.settings')
     try:
         from django.core.management import execute_from_command_line
