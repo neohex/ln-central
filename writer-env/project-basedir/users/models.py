921dc2e9769e2f13e7179358e1766a2de6a4ec17
Aleksandr Levchuk <alevchuk@gmail.com>
2019-03-25 20:31:06 +0000

    Add Profile and Tag to "users" models.py

--- writer-env/project-basedir/users/models.py
+++ writer-env/project-basedir/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.sites.models import Site
 from datetime import datetime
+from biostar_writer import const
 
 
 def now():
@@ -44,3 +45,111 @@ class User(models.Model):
 
     # The last visit by the user.
     last_login = models.DateTimeField(default=datetime.now)
+
+
+class Tag(models.Model):
+    name = models.TextField(max_length=50, db_index=True)
+
+
+class Profile(models.Model):
+    """
+    Maintains information that does not always need to be retreived whe a user is accessed.
+    """
+    LOCAL_MESSAGE, EMAIL_MESSAGE = const.LOCAL_MESSAGE, const.EMAIL_MESSAGE
+    NO_DIGEST, DAILY_DIGEST, WEEKLY_DIGEST, MONTHLY_DIGEST = range(4)
+    TYPE_CHOICES = const.MESSAGING_TYPE_CHOICES
+
+    DIGEST_CHOICES = [(NO_DIGEST, 'Never'), (DAILY_DIGEST, 'Daily'),
+                      (WEEKLY_DIGEST, 'Weekly'), (MONTHLY_DIGEST, 'Monthly')]
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+
+    # Globally unique id used to identify the user in a private feeds
+    uuid = models.CharField(null=False, db_index=True, unique=True, max_length=255)
+
+    # The last visit by the user.
+    last_login = models.DateTimeField()
+
+    # The last visit by the user.
+    date_joined = models.DateTimeField()
+
+    # User provided location.
+    location = models.CharField(default="", max_length=255, blank=True)
+
+    # User provided website.
+    website = models.URLField(default="", max_length=255, blank=True)
+
+    # This field is used to select content for the user.
+    my_tags = models.TextField(default="", max_length=255, blank=True)
+
+    # Description provided by the user html.
+    info = models.TextField(default="", null=True, blank=True)
+
+    # The default notification preferences.
+    message_prefs = models.IntegerField(choices=TYPE_CHOICES, default=const.LOCAL_MESSAGE)
+
+    # This stores binary flags on users. Their usage is to
+    # allow easy subselection of various subsets of users.
+    flag = models.IntegerField(default=0)
+
+    # The tag value is the canonical form of the post's tags
+    watched_tags = models.CharField(max_length=100, default="", blank=True)
+
+    # The tag set is built from the watch_tag string and is used to trigger actions
+    # when a post that matches this tag is set
+    tags = models.ManyToManyField(Tag, blank=True, )
+
+    # Subscription to daily and weekly digests.
+    digest_prefs = models.IntegerField(choices=DIGEST_CHOICES, default=WEEKLY_DIGEST)
+
+    # Opt-in to all messages from the site
+    opt_in = models.BooleanField(default=False)
+
+    def parse_tags(self):
+        return util.split_tags(self.tag_val)
+
+    def clear_data(self):
+        "Actions to take when suspending or banning users"
+        self.website = self.info = self.location = ''
+        self.save()
+
+    def add_tags(self, text):
+        text = text.strip()
+        # Sanitize the tag value
+        self.tag_val = bleach.clean(text, tags=[], attributes=[], styles={}, strip=True)
+        # Clear old tags
+        self.tags.clear()
+        tags = [Tag.objects.get_or_create(name=name)[0] for name in self.parse_tags()]
+        self.tags.add(*tags)
+
+    def save(self, *args, **kwargs):
+
+        # Clean the info fields.
+        self.info = bleach.clean(self.info, tags=ALLOWED_TAGS,
+                                 attributes=ALLOWED_ATTRIBUTES, styles=ALLOWED_STYLES)
+
+        # Strip whitespace from location string
+        self.location = self.location.strip()
+
+        if not self.id:
+            # This runs only once upon object creation.
+            self.uuid = util.make_uuid()
+            self.date_joined = self.date_joined or now()
+            self.last_login = self.date_joined
+
+        super(Profile, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return "%s" % self.user.pubkey
+
+    @property
+    def filled(self):
+        has_location = bool(self.location.strip())
+        has_info = bool(self.info.strip())
+        return has_location and has_info
+
+    @staticmethod
+    def auto_create(sender, instance, created, *args, **kwargs):
+        "Should run on every user creation."
+        if created:
+            prof = Profile(user=instance)
+            prof.save()
