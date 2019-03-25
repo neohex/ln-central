4b2b0fd7909640ecb8468073478e2dc1ab5f2cfa
Aleksandr Levchuk <alevchuk@gmail.com>
2019-03-25 20:35:32 +0000

    SECURE_HSTS_SECONDS=3600 for prod settings

--- writer-env/project-basedir/biostar_writer/settings/prod.py
+++ writer-env/project-basedir/biostar_writer/settings/prod.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+SECURE_HSTS_SECONDS=3600  # https://docs.djangoproject.com/en/2.1/ref/middleware/#http-strict-transport-security
+
 with open('/etc/biostar/django-secret') as django_secret:
     SECRET_KEY = django_secret.read().strip()
 
