0c8bfc7d6c04efb1e42576526fd88354fb24064c
Aleksandr Levchuk <alevchuk@gmail.com>
2019-11-28 09:43:42 -0800

    Simplify: Remove planet blogs

--- reader/project-basedir/biostar/server/search_indexes.py
+++ reader/project-basedir/biostar/server/search_indexes.py
@@ -9,7 +9,6 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='title')
     type = indexes.CharField(model_attr='type')
     content = indexes.CharField(model_attr='content')
-    author = indexes.CharField(model_attr='author__name')
 
     def get_model(self):
         return Post
@@ -21,21 +20,3 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_updated_field(self):
         return "lastedit_date"
-
-# Create the search indices.
-class BlogPostIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='get_title')
-    content = indexes.CharField(model_attr='html')
-    author = indexes.CharField(model_attr='blog__title')
-
-    def get_model(self):
-        return BlogPost
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        query = self.get_model().objects.all()
-        return query
-
-    def get_updated_field(self):
-        return "insert_date"
