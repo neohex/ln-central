6c6bce76f2299bb823fb52456f6ee8af9243b5df
Aleksandr Levchuk <alevchuk@gmail.com>
2019-12-03 07:49:30 -0800

    fix lner background task skeleton code

--- writer/project-basedir/lner/tasks.py
+++ writer/project-basedir/lner/tasks.py
@@ -11,7 +11,8 @@ logger = util.getLogger("lner.tasks")
 
 @background(queue='queue-1', remove_existing_tasks=True)
 def run():
-    invoices = lnclient.listinvoices(index_offset=4, mock=settings.MOCK_LN_CLIENT)
+    rpcserver = "tmpfake:123"
+    invoices = lnclient.listinvoices(index_offset=4, rpcserver=rpcserver, mock=settings.MOCK_LN_CLIENT)
     logger.info("Got {} invoices".format(len(invoices)))
 
 run(repeat=1)
