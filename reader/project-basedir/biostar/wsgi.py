a4137f69bf65eb4f12f21f3cd709964624015344
Aleksandr Levchuk <alevchuk@gmail.com>
2019-12-17 07:38:24 -0800

    Remove "whitenoise" static content lib that was not used

--- reader/project-basedir/biostar/wsgi.py
+++ reader/project-basedir/biostar/wsgi.py
@@ -6,10 +6,3 @@ from django.core.wsgi import get_wsgi_application
 
 # This is the default application
 application = get_wsgi_application()
-
-def white():
-    # This is an alternative WSGI app that wraps static content
-    from whitenoise.django import DjangoWhiteNoise
-    white = get_wsgi_application()
-    white = DjangoWhiteNoise(white)
-    return white
