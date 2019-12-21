b6aaa209d230b5942f391b6186df10c86967a7bb
Aleksandr Levchuk <alevchuk@gmail.com>
2019-12-21 14:40:54 +0000

    fix ~/l/biostar.sh writer-prod process_tasks

--- writer/project-basedir/lner/tasks.py
+++ writer/project-basedir/lner/tasks.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from background_task import background
 from common import lnclient
 from common.log import logger
-
+from lner.models import LightningNode
 
 @background(queue='queue-1', remove_existing_tasks=True)
 def run():
-    rpcserver = "tmpfake:123"
-    invoices = lnclient.listinvoices(index_offset=4, rpcserver=rpcserver, mock=settings.MOCK_LN_CLIENT)
-    logger.info("Got {} invoices".format(len(invoices)))
+    node = LightningNode.objects.get()
+    invoices = lnclient.listinvoices(index_offset=4, rpcserver=node.rpcserver, mock=settings.MOCK_LN_CLIENT)
+
+    # example invoices: {"invoices": [], 'first_index_offset': '5', 'last_index_offset': '72'}
+    logger.info("Got {} invoices".format(len(invoices['invoices'])))
 
 run(repeat=1)
