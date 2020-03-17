36a918ad0a854640d811ca84b8f21fb0cf254f20
Aleksandr Levchuk <alevchuk@gmail.com>
2020-03-17 08:27:27 -0700

    MAX_PAYREQ_SIZE can be larger

--- writer/project-basedir/biostar_writer/settings/base_common.py
+++ writer/project-basedir/biostar_writer/settings/base_common.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 PAYMENT_AMOUNT = 2  # in satoshi
 
 MAX_MEMO_SIZE = 600  # includes the length of the friendly prefix
-MAX_PAYREQ_SIZE = 1200  # pay_req includes memo and routing info
+MAX_PAYREQ_SIZE = 2000  # pay_req includes memo and routing info
 FRIENDLY_PREFIX = "ln.support"  # cannot have under-bars (_s)
 
 INVOICE_EXPIRY = 3600  # in seconds
