46273b97c75d8419a3b0a46f59de2781478f49b1
Aleksandr Levchuk <alevchuk@gmail.com>
2020-03-17 08:09:26 -0700

    Reduce max memo length to prevent errors like: "[lncli] rpc error: code = Unknown desc = data length too big to fit within 10 bits: 1228"

--- writer/project-basedir/biostar_writer/settings/base_common.py
+++ writer/project-basedir/biostar_writer/settings/base_common.py
@@ -6,8 +6,8 @@ from datetime import timedelta
 
 PAYMENT_AMOUNT = 2  # in satoshi
 
-MAX_MEMO_SIZE = 1000  # includes the length of the friendly prefix
-MAX_PAYREQ_SIZE = 2000  # pay_req includes memo and routing info
+MAX_MEMO_SIZE = 600  # includes the length of the friendly prefix
+MAX_PAYREQ_SIZE = 1200  # pay_req includes memo and routing info
 FRIENDLY_PREFIX = "ln.support"  # cannot have under-bars (_s)
 
 INVOICE_EXPIRY = 3600  # in seconds
